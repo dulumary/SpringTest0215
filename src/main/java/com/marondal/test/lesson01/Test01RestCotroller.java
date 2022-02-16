ca35d9efe3ab1da0009205a832cde8845f204c38
kim <kim@naver.com>
2022-02-16 17:58:55 +0900

    [김인규]  - 라이프 싸이클 문제 풀이

--- src/main/java/com/marondal/test/lesson01/Test01RestCotroller.java
+++ src/main/java/com/marondal/test/lesson01/Test01RestCotroller.java
@@ -5,13 +5,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marondal.test.lesson01.model.BoardContent;
+
 @RestController // @Controller + @ReponseBody
+@RequestMapping("/lesson01/test01")
 public class Test01RestCotroller {
 	
-	@RequestMapping("/lesson01/test01/3")
+	@RequestMapping("/3")
 	public List<Map<String, Object>> printMap() {
 		
 		List<Map<String, Object>> list = new ArrayList<>();
@@ -31,6 +36,44 @@ public class Test01RestCotroller {
 		list.add(map2);
 		
 		return list;
+		
+	}
+	
+	@RequestMapping("/4")
+	public List<BoardContent> printListObject() {
+		
+		List<BoardContent> list = new ArrayList<>();
+		
+		BoardContent boardContent1 = new BoardContent();
+		boardContent1.setTitle("안녕하세요. 가입인사 드립니다.");
+		boardContent1.setContent("안녕하세요 가입했어요 블라 블라~~");
+		boardContent1.setUser("김인규");
+		list.add(boardContent1);
+		
+		BoardContent boardContent2 = new BoardContent();
+		boardContent2.setTitle("헐 대박");
+		boardContent2.setContent("응 아니야~");
+		boardContent2.setUser("바다");
+		list.add(boardContent2);
+		
+		BoardContent boardContent3 = new BoardContent();
+		boardContent3.setTitle("오늘 여친 만난 썰 푼다");
+		boardContent3.setContent(".......");
+		boardContent3.setUser("dulumary");
+		list.add(boardContent3);
+		
+		return list;
+	}
+	
+	@RequestMapping("/5")
+	public ResponseEntity<BoardContent> entity() {
+		BoardContent boardContent1 = new BoardContent();
+		boardContent1.setTitle("안녕하세요. 가입인사 드립니다.");
+		boardContent1.setContent("안녕하세요 가입했어요 블라 블라~~");
+		boardContent1.setUser("김인규");
+		
+		ResponseEntity<BoardContent> entity = new ResponseEntity<>(boardContent1, HttpStatus.INTERNAL_SERVER_ERROR);
+		return entity;
 		
 	}
 
